8ee4e4e8ce37d30fd7801fe8937f391f656700fc
Joe Alongi <contact@joealongi.dev>
2026-06-03 20:27:04 -0400

    Retheme the apps

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Landing } from './pages/landing/landing';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '', component: Landing },
+  { path: 'book', component: Home },
   { path: 'dashboard', component: Dashboard },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, CommonModule],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.scss',
