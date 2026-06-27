5a52f0c01bcf9093d2f99d505c6061bb4de5672c
Joe Alongi <contact@joealongi.dev>
2026-06-26 22:42:33 -0400

    Cleanup routing

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -5,18 +5,7 @@ export const serverRoutes: ServerRoute[] = [
     path: '',
     renderMode: RenderMode.Prerender,
   },
-  {
-    path: 'privacy',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'terms',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'whitepaper',
-    renderMode: RenderMode.Prerender,
-  },
+
   {
     path: 'status',
     renderMode: RenderMode.Client,
@@ -37,18 +26,7 @@ export const serverRoutes: ServerRoute[] = [
     path: 'vulnerabilities',
     renderMode: RenderMode.Client,
   },
-  {
-    path: 'documentation',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'book',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'compliance',
-    renderMode: RenderMode.Client,
-  },
+
   {
     path: 'login',
     renderMode: RenderMode.Client,
